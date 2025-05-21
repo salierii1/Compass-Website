2fa91e054eca7aa1b2599770cffae6ed875eed0d
Your Name <yourname@example.com>
2025-05-21 23:37:29 +0800

    new home (2)

--- home.php
+++ home.php
@@ -64,7 +64,7 @@ body {
   background: 
     linear-gradient(to bottom, rgba(255,255,255,0) 50%, #fff 100%),
     url('https://images4.alphacoders.com/656/656354.jpg') no-repeat center center/cover;
-  height: 70vh;
+  height: 100vh;
   position: relative;
 }
 
